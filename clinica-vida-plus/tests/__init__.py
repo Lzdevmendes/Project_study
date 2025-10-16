e4b74720e08de9471aed05cc4c5ccce0f97d0653
Lzmendesdev <Lzmendestechdev@gmail.com>
2025-10-16 20:16:32 -0300

    Feat: Create file .gitkeep

---
+++ clinica-vida-plus/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Testes para o Sistema de Gestão - Clínica Vida+
+
+Este pacote contém todos os testes unitários e de integração
+para os módulos do sistema.
+
+Author: Sistema Clínica Vida+
+Date: 2025-10-15
+"""
